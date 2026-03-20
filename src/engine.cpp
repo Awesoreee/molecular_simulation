168a820cf6a3794634a62f93212937016af2859c
tomigras <tomasgrish@gmail.com>
2026-03-21 01:04:27 +0300

    work in progress

--- src/engine.cpp
+++ src/engine.cpp
@@ -6,7 +6,7 @@
 class atoms {
     sf::Vector2u borders;
     float sigmaNe = 2.67;
-    float epsNe = 1;
+    float epsNe = 0.0031;
     std::vector<sf::Vector2f> veloc_all;
     std::vector<sf::Vector2f> forces_all;
 
@@ -18,18 +18,22 @@ class atoms {
         borders = bord;
         coords_all.resize(n);
         veloc_all.resize(n);
+        forces_all.resize(n);
         amount = n;
     }
     int add_atom(sf::Vector2f coords = sf::Vector2f(0.f, 0.f), sf::Vector2f veloc = sf::Vector2f(0.f, 0.f)){
         coords_all.push_back(coords);
         veloc_all.push_back(veloc);
+        forces_all.push_back({1,1});
         amount++;
     }
     int tick_forward(float dt){
         count_forces();
         int i;
-        for (i=0;i++;i<amount){
+        for (i=0;i<amount;i++){
             coords_all[i] += veloc_all[i] * dt + 0.5f * forces_all[i] * dt * dt;
+            veloc_all[i] += forces_all[i] * dt;
+            std::cout << i << "coords and veloc" << coords_all[i].x << coords_all[i].y << veloc_all[i].x << veloc_all[i].y << std::endl;
         }
         return 0;
     }
@@ -38,13 +42,16 @@ private:
         sf::Vector2f diff = coords1 - coords2;
         float distance = std::sqrt(diff.x * diff.x + diff.y * diff.y);
         float force = (48 * epsNe / sigmaNe) * (pow(sigmaNe / distance, 13) - 0.5 * pow(sigmaNe / distance, 7));
+        std::cout << "Distance: " << distance << ", Force: " << force << std::endl;
         return diff.normalized() * force;
     }
     int count_forces(){
         int i, j;
-        for (i=0;i++;i<amount){
-            for (j=0;j++;j<amount){
+        for (i=0;i<amount;i++){
+            forces_all[i] = {0,0};
+            for (j=0;j<amount;j++){
                 if (i != j) {
+                    std::cout << i << j;
                     forces_all[i] += count_force(coords_all[i], coords_all[j]);
                 }
             }
