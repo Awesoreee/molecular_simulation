897ac5ddd3b473514c71ad4c63306785f663841d
tomigras <tomasgrish@gmail.com>
2026-04-03 22:29:51 +0300

    borders fix

--- src/engine.cpp
+++ src/engine.cpp
@@ -9,6 +9,7 @@
 class atoms {
     std::random_device rd;
     std::mt19937 gen{rd()};
+    sf::Vector2u zeroPos;
     sf::Vector2u borders;
     float sigmaNe = 2.74;
     float epsNe = 0.0031 * 1.602 * pow(10, -19);
@@ -21,7 +22,8 @@ class atoms {
     std::vector<sf::Vector2f> coords_all;
     int amount = 0;
 
-    atoms(int n, sf::Vector2u bord){
+    atoms(int n, sf::Vector2u zero, sf::Vector2u bord){
+        zeroPos = zero;
         borders = bord;
         coords_all.resize(n);
         veloc_all.resize(n);
@@ -51,8 +53,8 @@ class atoms {
 
     void randomize(int max_velocity){
         int i;
-        std::uniform_real_distribution<float> distx(0.0f,float(borders.x));
-        std::uniform_real_distribution<float> disty(0.0f,float(borders.y));
+        std::uniform_real_distribution<float> distx(float(zeroPos.x),float(zeroPos.x + borders.x));
+        std::uniform_real_distribution<float> disty(float(zeroPos.y),float(zeroPos.y + borders.y));
         std::uniform_real_distribution<float> distv(0.0f,float(2*max_velocity));
         for (i = 0; i < amount; i++){
             coords_all[i] = {distx(gen), disty(gen)};
@@ -88,18 +90,18 @@ private:
     int check_wall_collision(sf::Vector2f* coords, sf::Vector2f* veloc, sf::Vector2f force, float mass, float dt) {
         *coords += *veloc * dt + 0.5f * force * dt * dt / mass;
         *veloc += force * dt / mass;
-        if (coords->x < 0) {
-            coords->x = -coords->x;
+        if (coords->x < zeroPos.x) {
+            coords->x = 2.0f * zeroPos.x - coords->x;
             if (veloc->x < 0) veloc->x *= -1;
-        } else if (coords->x > borders.x) {
-            coords->x = 2.0f * borders.x - coords->x; 
+        } else if (coords->x > zeroPos.x + borders.x) {
+            coords->x = 2.0f * (zeroPos.x + borders.x) - coords->x; 
             if (veloc->x > 0) veloc->x *= -1;
         }
-        if (coords->y < 0) {
-            coords->y = -coords->y; 
+        if (coords->y < zeroPos.y) {
+            coords->y = 2.0f * zeroPos.y - coords->y; 
             if (veloc->y < 0) veloc->y *= -1;
-        } else if (coords->y > borders.y) {
-            coords->y = 2.0f * borders.y - coords->y;
+        } else if (coords->y > zeroPos.y + borders.y) {
+            coords->y = 2.0f * (zeroPos.y + borders.y) - coords->y;
             if (veloc->y > 0) veloc->y *= -1;
         }
         return 0;
